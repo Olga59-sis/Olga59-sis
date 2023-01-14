500686e69bef33285a039cadd7528131bc151caa
Olga59-sis <aglokazachinskaya@gmail.com>
2023-01-14 20:31:04 +0300

    Добавление поля результата

--- 12_Calculator.py
+++ 12_Calculator.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+#Python Калькулятор
+
 window=tk.Tk()
 window. geometry("300x200")
 window. title("Python калькулятор")
@@ -8,10 +10,14 @@ first = tk.Label(text = "Первое число")
 first.grid(column=0, row=0, padx=10, pady=10)
 second = tk.Label(text = "Второе число")
 second.grid(column=0, row=1)
+result = tk.Label(text = "Результаты")
+result.grid(column=0, row=2, padx=10, pady=10)
 
 firstEntry = tk.Entry()
 firstEntry.grid(column=1, row=0)
 secondEntry = tk.Entry()
 secondEntry.grid(column=1, row=1)
+resultLabel = tk.Label(text = "")
+resultLabel.grid(column=1, row=2)
 
 window. mainloop()
