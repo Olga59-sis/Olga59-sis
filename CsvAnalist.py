21190b55830410d6ac70ca6520c547d0536fa7dc
Olga59-sis <aglokazachinskaya@gmail.com>
2023-01-20 23:08:15 +0300

    имя текущей папки и вывода файла

--- CsvAnalist.py
+++ CsvAnalist.py
@@ -36,7 +36,8 @@ output_text.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
 # Диалог открытия файла
 def do_dialog():
-    name= fd.askopenfilename()
+    my_dir = os.getcwd()
+    name= fd.askopenfilename(initialdir=my_dir)
     return name
 
 #Обрабочик нажатия кнопки
