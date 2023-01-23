2f3e39c9d33473c4d281a644a2ae14995db42c2b
Olga59-sis <aglokazachinskaya@gmail.com>
2023-01-23 19:54:21 +0300

    Дополнение поиска имени в списке

--- CsvAnalist.py
+++ CsvAnalist.py
@@ -51,7 +51,7 @@ def pandas_read_csv(file_name):
     Label_21['text'] = cnt_columns
     return df
     
-# Выборка стобца в списке
+# Выборка стобца в списok
 def get_column(df, column_ix):
     cnt_rows = df.shape[0]
     lst = []
@@ -82,21 +82,20 @@ def list_meet_name(fields_list):
      # Конец подсчета
         if counter_meet /counter_total > 0.1:
          return True
- #Не набралось нужного коллчества совпадений 
+ #Не набралось нужного колличества совпадений 
     return False
            
-            
-
 #Обработчик нажатия кнопки
 def process_button():
     file_name = do_dialog()
     Label_01['text'] = file_name
     df = pandas_read_csv(file_name)
     lst = get_column(df, 2)
-    for item in lst:
-        output_text.insert(tk.END, str(item) + '   '
-            + str(meet_name(item)) + os. linesep)
-        mb.showinfo(title=None, message= "Готово")
+    if list_meet_name(lst):
+        output_text.insert(tk.END, "В списке предположительно содержится имя." + os. linesep)
+    else:
+        output_text.insert(tk.END, "Предположений для списка не найдено" + os. linesep)     
+    mb.showinfo(title=None, message="Готово"    
     
 #Создание кнопки
 button=tk.Button(window, text="Прочитать файл", command=process_button)
