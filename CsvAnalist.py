ad221290e8cf22bc30a4ea29cc00abb433083826
Olga59-sis <aglokazachinskaya@gmail.com>
2023-01-30 10:24:50 +0300

    Добавление анализа amail

--- CsvAnalist.py
+++ CsvAnalist.py
@@ -110,6 +110,31 @@ def list_meet_date_am_pm(fields_list):
     #Не набралось нужного количества совпадений 
     return False, ratio
     
+# Если в этом поле фамилия, пусть вернет True         
+def meet_amail(field):
+    checkfor = ['@']
+    for s in checkfor:
+        if s in str(field): # Нашлось!
+            return True
+    #Ничего не совпало
+    return False
+    
+# Если в этом списке многие элементы содержат фамилия, пусть вернет True
+def list_meet_amail(fields_list):
+    counter_total = 0
+    counter_meet = 0
+    for list_item in fields_list:
+        counter_total += 1
+        if meet_amail(list_item):
+            counter_meet += 1
+    
+    # Конец подсчета
+    ratio = counter_meet / counter_total
+    if ratio  > 0.1:
+        return True, ratio
+    #Не набралось нужного количества совпадений 
+    return False, ratio
+    
     
 # Пройти все столбцы
 def check_all_columns(df):
@@ -135,9 +160,14 @@ def check_all_columns(df):
                 + "%." + os.linesep + os.linesep)
             continue # Все нашли, можно идти к следующему столбцу    
                     
-        #Соответствия критериям не найдено      
-            output_text.insert(tk.END, "Предположений для столбца " + str(i+1)
-                + " не найдено." + os.linesep + os.linesep) 
+        # Третий критерий
+        result3 = list_meet_amail(lst)
+        if result3[0]:
+            output_text.insert(tk.END,"В столбце " + str(i+1)  
+                + " предположительно содержится фамилия." + os.linesep)
+            output_text.insert(tk.END,"Процент совпадений " + "{:.2f}".format(result3[1]*100)
+                + "%." + os.linesep + os.linesep)  
+            continue # Все нашли, можно идти к следующему столбцу         
             
 
 #Обработчик нажатия кнопки
